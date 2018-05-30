33caa879d57b5b5b08fbb32b693953def1df32b7
Yanzhenqing <zhenqingyan@126.com>
2018-05-30 23:32:42 +0800

    1

--- YouModel/DataModel/OrderInfo.cs
+++ YouModel/DataModel/OrderInfo.cs
@@ -23,58 +23,58 @@ namespace YouModel.DataModel
         /// 订单号
         /// </summary>
         public string SerialId { get; set; }
-        /// <summary>
-        /// 项目号
-        /// </summary>
-        public string ProjectNo { get; set; } = string.Empty;
-        /// <summary>
-        /// 凭证日期
-        /// </summary>
-        public DateTime VoucherDate { get; set; } = DateTime.Now;
-        /// <summary>
-        /// 交货日期
-        /// </summary>
-        public DateTime DeliveryDate { get; set; } = DateTime.Now;
-        /// <summary>
-        /// 物料
-        /// </summary>
-        public string Materiel { get; set; } = string.Empty;
-        /// <summary>
-        /// 图号
-        /// </summary>
-        public string FigureNo { get; set; } = string.Empty;
-        /// <summary>
-        /// 版本
-        /// </summary>
-        public string Version { get; set; } = string.Empty;
-        /// <summary>
-        /// 短文本
-        /// </summary>
-        public string ShortText { get; set; } = string.Empty;
-        /// <summary>
-        /// 数量
-        /// </summary>
-        public int Number { get; set; } = 1;
-        /// <summary>
-        /// 变式
-        /// </summary>
-        public string VariableType { get; set; } = string.Empty;
-        /// <summary>
-        /// 变式公式
-        /// </summary>
-        public string VariableFormula { get; set; } = string.Empty;
-        /// <summary>
-        /// 检测控制计划
-        /// </summary>
-        public string TestingPlan { get; set; } = string.Empty;
-        /// <summary>
-        /// 是否开票
-        /// </summary>
-        public string IsTicket { get; set; } = string.Empty;
-        /// <summary>
-        /// 友华交期
-        /// </summary>
-        public DateTime YouhuaDate { get; set; } = DateTime.Now;
+        ///// <summary>
+        ///// 项目号
+        ///// </summary>
+        //public string ProjectNo { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 凭证日期
+        ///// </summary>
+        //public DateTime VoucherDate { get; set; } = DateTime.Now;
+        ///// <summary>
+        ///// 交货日期
+        ///// </summary>
+        //public DateTime DeliveryDate { get; set; } = DateTime.Now;
+        ///// <summary>
+        ///// 物料
+        ///// </summary>
+        //public string Materiel { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 图号
+        ///// </summary>
+        //public string FigureNo { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 版本
+        ///// </summary>
+        //public string Version { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 短文本
+        ///// </summary>
+        //public string ShortText { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 数量
+        ///// </summary>
+        //public int Number { get; set; } = 1;
+        ///// <summary>
+        ///// 变式
+        ///// </summary>
+        //public string VariableType { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 变式公式
+        ///// </summary>
+        //public string VariableFormula { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 检测控制计划
+        ///// </summary>
+        //public string TestingPlan { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 是否开票
+        ///// </summary>
+        //public string IsTicket { get; set; } = string.Empty;
+        ///// <summary>
+        ///// 友华交期
+        ///// </summary>
+        //public DateTime YouhuaDate { get; set; } = DateTime.Now;
         /// <summary>
         /// 备注
         /// </summary>
